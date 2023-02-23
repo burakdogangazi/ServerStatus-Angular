7bfa346d8c1d5375cbf4e5472955101bf85e0491
Burak Doğan <ddoganbburak@gmail.com>
2023-02-23 19:09:50 +0300

    Angular Intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles:[]
+  //template :[] bu tanımlarda içeriye kod yazabiliyoruz.
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Burak';
 }
